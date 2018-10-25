629f07a3f91304705b94b8f6c24f619dcf577233
R! <44431002+r-not@users.noreply.github.com>
2018-10-25 21:55:29 +0600

    Update ubf-install.sh

--- ubf-install.sh
+++ ubf-install.sh
@@ -1,5 +1,5 @@
 #!
 cd /tmp
 wget --no-check-certificate https://github.com/r-not/unibnfonts/archive/master.tar.gz -O ubf.tar.gz
-tar  -xvf ubf.tar.gz  -C /usr/share/fonts/
+tar -xvf ubf.tar.gz -C /usr/share/fonts/
 rm ubf.tar.gz
